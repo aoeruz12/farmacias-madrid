e2c571b1bfd3658daca80734f8d343f530eac784
Tu Nombre <tu@email.com>
2026-09-13 09:28:25 -0600

    Quitar ORDER BY duplicado

--- app.py
+++ app.py
@@ -168,7 +168,6 @@ def get_productos():
             query += ' AND categoria = ?'
             params.append('Nacional')
             query += ' ORDER BY prioridad DESC, id_producto ASC'
-            query += ' ORDER BY prioridad DESC, id_producto ASC'
         else:
             query += ' AND categoria = ?'
             params.append('Importación')
