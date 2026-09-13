a320df617e030c99343dc6c36b7b5aa12d7e29d7
Tu Nombre <tu@email.com>
2026-09-13 08:35:38 -0600

    Tailwind compilado para produccion

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./*.html",
+    "./templates/**/*.html",
+    "./static/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
