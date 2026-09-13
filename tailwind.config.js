26b1dcb62b641de5752981ee66487c5a157ab2fe
Tu Nombre <tu@email.com>
2026-09-13 08:46:01 -0600

    Quitar tailwind.config del HTML y mover a config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ module.exports = {
     "./static/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        madrid: {
+          blue: '#0A398A',
+          darkblue: '#052258',
+          yellow: '#FFC000',
+          yellowhover: '#E5AC00',
+          emerald: '#10B981',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
